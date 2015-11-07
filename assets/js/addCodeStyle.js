09c0254d15fc5b4bcae0a3cbe759ebf5fe1a4add
Elena Torro <elenatorro@gmail.com>
2015-11-07 11:50:07 +0100

    code highlight fix

--- assets/js/addCodeStyle.js
+++ assets/js/addCodeStyle.js
@@ -2,8 +2,8 @@ $(function() {
 	var addCodeStyle = function() {
 	var item;
 	var element;
-		for (item in document.getElementsByTagName('pre')) {
-			element = document.getElementsByTagName('pre')[item];
+		for (item in document.getElementsByTagName('code')) {
+			element = document.getElementsByTagName('code')[item];
 			if (!element.className) {
 				element.className = 'language-html';
 			}
